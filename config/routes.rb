301e53c92814da1daebc0ef7e5b7a961b64c3f5e
Sova171 <vladkuzak@gmail.com>
2023-04-10 22:45:26 +0300

    add gallery

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'articles/index'
+  get 'gallery/index'
   scope '(:locale)', locale: /en|ua/ do
     root 'main#index'
     resources :about
     resources :articles
+    resources :gallery
   end
 end
